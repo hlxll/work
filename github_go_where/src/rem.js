a4f398cfce92f05acd95858921c9a08735ec6f16
huanglin <huanglin@MacBook-Pro.local>
2020-06-28 13:40:55 +0800

    改动-G样式

--- github_go_where/src/rem.js
+++ github_go_where/src/rem.js
@@ -1,12 +1,29 @@
 (function (doc, win) {
 
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["Android", "iPhone","SymbianOS", "Windows Phone", "iPod"];
+    var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+    if(window.screen.width>=768){
+         flag = true;
+    }
+
       var docEl = doc.documentElement,
         resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
     
         recalc = function () {
             var clientWidth = docEl.clientWidth;
             if (!clientWidth) return;
-            // console.log(clientWidth)屏幕宽度
+
+            if(flag && clientWidth<=1100){
+                clientWidth =1100 
+            }
+
             docEl.style.fontSize = 50 * (clientWidth / 375) + 'px';
             // console.log(docEl.style.fontSize)//根据屏幕宽度设置根元素字体大小
         };
