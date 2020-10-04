f0c1b45f3d485ab0018307e369d9058ceddc6bd3
huanglin <huanglin@MacBook.local>
2020-10-04 19:39:49 +0800

    webpack

--- github_go_where/build/webpack.base.conf.js
+++ github_go_where/build/webpack.base.conf.js
@@ -36,28 +36,48 @@ module.exports = {
     hashDigest: 'hex',//生成hash时候使用的编码方式
   },
   resolve: {
-    //自动解析，在引入时候可以不带扩展名
-    extensions: ['.js', '.vue', '.json'],
-    alias: {
+    alias: {//import或require的别名，让模块引入更简单
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
       //让引入模块更简单，比如static文件夹下得东西，引入时候可以不加类似../，直接加static别名
       static: path.resolve(__dirname, 'static')
-    }
+    },
+    aliasFields: ["browser"],//解析的规范，默认broeser
+    descriptionFiles: ["package.json"],//描述的json文件，
+    enforceExtension: false,//设置true，引入文件必须带扩展名的才可以正常工作，默认false可以不带扩展，引入也可以工作
+    enforceModuleExtension: false,//对模块扩展要求
+    extensions: ['.js', '.vue', '.json'],//自动解析确定的扩展，引入时候可以不带扩展
+    mainFields: ["browser", "module", "main"],//设置package。json中的导入模块顺序，package中会定义browser，module，main三个属性的地址
+                                      //通过这里的定义，确定package从哪个字段默认解析文件
+    mainFiles: ["index"],//解析目录时要使用的文件名
+    // modules: [path.resolve(__dirname, "src"), "node_modules"]，解析模块设置的目录，自己添加一个目录到模块搜索目录，此目录优先于node_modules
+    // unsafeCache: //可以用布尔值或者正则，用于匹配文件路径或者缓存某些模块
+    plugins:[
+      
+    ]
+    
   },
   module: {
     noParse: function(content){//忽略正则匹配的文件，不解析，忽略的文件中不应该含有import， require，define的调用，或任何其他调用机制
       return /jquery|lodash/.test(content);
     },
     rules: [
+      //issuer被请求资源的模块文件的绝对路径，导入时的位置，resource请求文件的绝对路径
+      //oneOf规则数组，当匹配时，只使用第一个规则
+      //options和query是Rule.use:[{options}]的简写
       ...(config.dev.useEslint ? [createLintingRule()] : []),
       {
         test: /\.scss$/,
-        loaders: ['style', 'css', 'sass']
+        // oneOf:[{resourceQuery: /inline/,use: 'url-loader'},{}],
+        loaders: ['style', 'css', 'sass'],//Rule.use别名
+        parser:{//解析器，定义是否使用
+          amd:false,
+          commonjs:false
+        }
       },
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
+        loader: 'vue-loader',//Rule.use:[{loader}]简写
         options: vueLoaderConfig
       },
       {
